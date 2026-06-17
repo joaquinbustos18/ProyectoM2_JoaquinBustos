36a6eed71fcb6d28fb2f73174ae97ad1cf7e2295
Joaquin Bustos <joaquinnicolasbustos.com>
2026-06-17 03:58:28 -0300

    fix: usar DATABASE_URL para conexion en produccion

--- src/db/db.js
+++ src/db/db.js
@@ -2,12 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.PGUSER || process.env.DB_USER,
-  host: process.env.PGHOST || process.env.DB_HOST,
-  database: process.env.PGDATABASE || process.env.DB_NAME,
-  password: process.env.PGPASSWORD || process.env.DB_PASSWORD,
-  port: process.env.PGPORT || process.env.DB_PORT,
-  ssl: process.env.PGHOST ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("railway")
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
