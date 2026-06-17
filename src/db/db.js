b37d9e41ad51291754a4ea540b8181a5a63e6ccb
Joaquin Bustos <joaquinnicolasbustos.com>
2026-06-17 03:39:00 -0300

    Configurar PostgreSQL para Railway

--- src/db/db.js
+++ src/db/db.js
@@ -2,11 +2,12 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER || process.env.DB_USER,
+  host: process.env.PGHOST || process.env.DB_HOST,
+  database: process.env.PGDATABASE || process.env.DB_NAME,
+  password: process.env.PGPASSWORD || process.env.DB_PASSWORD,
+  port: process.env.PGPORT || process.env.DB_PORT,
+  ssl: process.env.PGHOST ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
